14305d734d4d71403f1d7828f67b3e440d502ab0
竹逊 <zhuxun.lx@alibaba-inc.com>
2016-09-17 20:20:14 +0800

    debug

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $tutorials = $tutorialService->selectTutorialsLastN(3);
 	<meta charset="utf-8" />
 	<meta name="Keywords" content="Phaser，Phaser小站，HTML5，游戏，HTML5游戏，游戏引擎，游戏框架，HTML5游戏引擎，HTML5游戏框架" />
 	<meta name="Description" content="Phaser小站是一个为phaser而生的网站，以宣传和发展Phaser为使命。本站提供一切关于Phaser的案例、源码、教程、教学视频。" />
+	<meta name="baidu-site-verification" content="W0Gy0sAtiL" />
 	<link href="css/global.css" rel="stylesheet" />
 	<link href="css/common.css" rel="stylesheet" />
 	<link href="css/index.css" rel="stylesheet" />
