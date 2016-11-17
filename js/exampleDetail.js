250b000aa0d6f7bb72ab0f7ac4a99470723e4536
竹逊 <zhuxun.lx@alibaba-inc.com>
2016-11-17 12:36:01 +0800

    click to load resource

--- js/exampleDetail.js
+++ js/exampleDetail.js
@@ -2,4 +2,12 @@
 	
 	Sunlight.highlightAll();
 	
+	var assets = $("span.sunlight-string:contains('assets')");
+	assets.addClass('hand');
+	assets.on("click", function() {
+		$this = $(this);
+		var text = $this.text().replace(/'/g, "");
+		window.open(text, '_blank');
+	});
+	
 })(jQuery);
