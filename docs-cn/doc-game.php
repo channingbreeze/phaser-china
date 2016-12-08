bb238deb5478c461becdc79aa553236dd13f72bc
竹逊 <zhuxun.lx@alibaba-inc.com>
2016-12-08 19:26:42 +0800

    docs

--- docs-cn/doc-game.php
+++ docs-cn/doc-game.php
@@ -8,14 +8,14 @@
 <p class="subTitle">参数：</p>
 <table cellspacing="0">
 	<tr align="left"><th width="12%" class="indentS">参数名</th><th width="12%">参数类型</th><th width="12%">是否可选</th><th width="12%">默认值</th><th width="42%">描述</th></tr>
-	<tr class="td"><td class="params indentS">width</td><td>数值 或 字符串</td><td>可选</td><td>800</td><td>游戏宽度。可以用0-100的字符串指定宽度为父元素的百分比。</td></tr>
-	<tr class="td"><td class="params indentS">height</td><td>数值 或 字符串</td><td>可选</td><td>600</td><td>游戏高度。可以用0-100的字符串指定高度为父元素的百分比。</td></tr>
-	<tr class="td"><td class="params indentS">renderer</td><td>枚举</td><td>可选</td><td>Phaser.AUTO</td><td>游戏渲染器。Phaser.AUTO(自动选择)，Phaser.WEBGL, Phaser.CANVAS，Phaser.HEADLESS(不渲染)。<span class="warning">手机游戏请务必指定Phaser.CANVAS</span></td></tr>
-	<tr class="td"><td class="params indentS">parent</td><td>字符串 或 HTML元素</td><td>可选</td><td>''</td><td>游戏的父DOM元素，或者DOM元素的ID</td></tr>
-	<tr class="td"><td class="params indentS">state</td><td>对象</td><td>可选</td><td>null</td><td>游戏初始场景，一个包含<a>Phaser.State</a>函数(preload, create, update, render)的对象</td></tr>
-	<tr class="td"><td class="params indentS">transparent</td><td>布尔</td><td>可选</td><td>false</td><td>是否要一个透明背景</td></tr>
-	<tr class="td"><td class="params indentS">antialias</td><td>布尔</td><td>可选</td><td>true</td><td>是否要抗锯齿效果。如果要实现像素化的复古风格游戏，可以置为false</td></tr>
-	<tr class="td"><td class="params indentS">physicsConfig</td><td>对象</td><td>可选</td><td>null</td><td>物理引擎的配置对象</td></tr>
+	<tr class="td"><td class="params indentS">width</td><td>number|string</td><td>可选</td><td>800</td><td>游戏宽度。可以用0-100的字符串指定宽度为父元素的百分比。</td></tr>
+	<tr class="td"><td class="params indentS">height</td><td>number|string</td><td>可选</td><td>600</td><td>游戏高度。可以用0-100的字符串指定高度为父元素的百分比。</td></tr>
+	<tr class="td"><td class="params indentS">renderer</td><td>number</td><td>可选</td><td>Phaser.AUTO</td><td>游戏渲染器。Phaser.AUTO(自动选择)，Phaser.WEBGL, Phaser.CANVAS，Phaser.HEADLESS(不渲染)。<span class="warning">手机游戏请务必指定Phaser.CANVAS</span></td></tr>
+	<tr class="td"><td class="params indentS">parent</td><td>string|DOM</td><td>可选</td><td>''</td><td>游戏的父DOM元素，或者DOM元素的ID</td></tr>
+	<tr class="td"><td class="params indentS">state</td><td>object</td><td>可选</td><td>null</td><td>游戏初始场景，一个包含<a>Phaser.State</a>函数(preload, create, update, render)的对象</td></tr>
+	<tr class="td"><td class="params indentS">transparent</td><td>boolean</td><td>可选</td><td>false</td><td>是否要一个透明背景</td></tr>
+	<tr class="td"><td class="params indentS">antialias</td><td>boolean</td><td>可选</td><td>true</td><td>是否要抗锯齿效果。如果要实现像素化的复古风格游戏，可以置为false</td></tr>
+	<tr class="td"><td class="params indentS">physicsConfig</td><td>object</td><td>可选</td><td>null</td><td>物理引擎的配置对象</td></tr>
 </table>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-25">line 25</a><p>
 
@@ -58,11 +58,6 @@
 <p><span class="keywords">create</span> : <a>Phaser.Create</a></p>
 <p>资源生成器</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-266">line 266</a><p>
-
-<p><span class="keywords">&lt;internal&gt;currentUpdateID</span> : integer</p>
-<p>渲染帧时候的ID，从0开始。比如第一帧是 currentUpdateID === 0 最后一帧是 currentUpdateID === updatesThisFrame</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-336">line 336</a><p>
 
 <p><span class="keywords">debug</span> : <a>Phaser.Utils.Debug</a></p>
 <p>一系列很有用的调试工具集</p>
@@ -169,16 +164,6 @@
 <p>此值决定绘制的缓存在渲染之后要不要保留。</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-108">line 108</a><p>
 
-<p><span class="keywords">&lt;internal&gt;raf</span> : <a>Phaser.RequestAnimationFrame</a></p>
-<p>用requestAnimationFrame或者setTimeout来处理游戏主循环。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-151">line 151</a><p>
-
-<p><span class="keywords">&lt;internal&gt;renderer</span> : <a>PIXI.CanvasRenderer</a>|<a>PIXI.WebGLRenderer</a></p>
-<p>PIXI渲染器。Phaser是基于PIXI引擎的。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-122">line 122</a><p>
-
 <p><span class="keywords">&lt;readonly&gt;renderType</span> : number</p>
 <p>Phaser将使用的渲染器类型。可以是Phaser.AUTO, Phaser.CANVAS 或者 Phaser.WEBGL</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-128">line 128</a><p>
@@ -228,11 +213,6 @@
 <p>tween动画管理器的引用。</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-211">line 211</a><p>
 
-<p><span class="keywords">&lt;internal&gt;updatesThisFrame</span> : integer</p>
-<p>渲染一帧的时候，发生的update的次数。理论上是1，除非出现跳帧和补帧。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-343">line 343</a><p>
-
 <p><span class="keywords">&lt;readonly&gt;width</span> : integer</p>
 <p>游戏的宽度。不要直接更改此值，请使用<a>Phaser.ScaleManager#setGameSize</a>，比如<span class="params">game.scale.setGameSize(width, height)</span></p>
 <p>默认值：800</p>
@@ -243,11 +223,6 @@
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-216">line 216</a><p>
 
 <p class="title">方法</p>
-
-<p><span class="keywords">&lt;internal&gt;boot()</span></p>
-<p>初始化游戏引擎的子模块，并且开始游戏。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-520">line 520</a><p>
 
 <p><span class="keywords">destroy()</span></p>
 <p>销毁整个游戏。</p>
@@ -261,87 +236,8 @@
 <p>允许单步调试。可以通过game.step()来驱动游戏运行。调用一次step，游戏循环前进一帧。这个方法在调试一些棘手的问题是非常有用。</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-956">line 956</a><p>
 
-<p><span class="keywords">&lt;internal&gt;focusGain(event)</span></p>
-<p>舞台获取焦点时被调用</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">event</td><td>object</td><td>获取焦点时的DOM事件。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-1100">line 1100</a><p>
-
-<p><span class="keywords">&lt;internal&gt;focusGain(event)</span></p>
-<p>舞台失去焦点时被调用</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">event</td><td>object</td><td>失去焦点时的DOM事件。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-1082">line 1082</a><p>
-
-<p><span class="keywords">&lt;internal&gt;gamePaused(event)</span></p>
-<p>游戏暂停时被调用</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">event</td><td>object</td><td>导致游戏暂停的DOM事件。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-1029">line 1029</a><p>
-
-<p><span class="keywords">&lt;internal&gt;gameResumed(event)</span></p>
-<p>游戏恢复时被调用</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">event</td><td>object</td><td>导致游戏恢复的DOM事件。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-1055">line 1055</a><p>
-
-<p><span class="keywords">&lt;internal&gt;parseConfig()</span></p>
-<p>解析游戏配置对象</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-440">line 440</a><p>
-
-<p><span class="keywords">&lt;internal&gt;setUpRenderer()</span></p>
-<p>检查设备能够使用指定的渲染器，如果不能，就替换渲染器。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-676">line 676</a><p>
-
-<p><span class="keywords">&lt;internal&gt;showDebugHeader()</span></p>
-<p>在控制台中显示Phaser版本等调试信息。</p>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-609">line 609</a><p>
-
 <p><span class="keywords">step()</span></p>
 <p>启动单步调试模式的时候，直接调用这个方法，可以让游戏前进一帧。这在调试一些棘手问题时非常有效，可以配合stepCount来查看游戏进行的步数。</p>
 <p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-982">line 982</a><p>
-
-<p><span class="keywords">&lt;internal&gt;update(time)</span></p>
-<p>游戏主循环</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">time</td><td>number</td><td>由RequestAnimationFrame提供的当前时间。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-770">line 770</a><p>
-
-<p><span class="keywords">&lt;internal&gt;updateLogic(timeStep)</span></p>
-<p>update Phaser中的所有逻辑子系统。被Game.update调用。</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">timeStep</td><td>number</td><td>由Game.update传过来的当前时间。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-876">line 876</a><p>
-
-<p><span class="keywords">&lt;internal&gt;updateRender(elapsedTime)</span></p>
-<p>渲染循环。渲染流程：首先调用State.preRender，在State.preRender中，你可以在渲染前对你的对象做最后的调整。然后通过渲染器，渲染整个对象列表，从Stage开始渲染，直到最后一个对象。然后开始渲染插件，根据插件被enable的顺序进行依次渲染。然后调用State.render，在State.render中发生的渲染将会置于顶层，覆盖之前渲染的对象。最后，插件按顺序被调用plugin.postRender。</p>
-<p>这个方法会自动被Game.update调用，我们一般不手动调用它。你可以用Game.lockRender来控制渲染，只有Game.lockRender为false的时候，Phaser才会进行渲染。</p>
-<table cellspacing="0">
-	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
-	<tr class="td"><td class="params indentS">elapsedTime</td><td>number</td><td>距上一次update的时间。</td></tr>
-</table>
-<p>这是内部值，以后可能被移除。</p>
-<p class="source indentS">源码 - <a href="../docs/src_core_Game.js.html">core/Game.js</a>, <a href="../docs/src_core_Game.js.html#sunlight-1-line-922">line 922</a><p>
 
 <?php include_once 'partials/docFoot.php'; ?>
--- docs-cn/doc-loader.php
+++ docs-cn/doc-loader.php
@@ -43,6 +43,157 @@
 <p>如果你想在所有资源前面添加一个URL，可以设置此参数。这个参数必须以/结尾</p>
 <p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-83">line 83</a><p>
 
+<p><span class="keywords">crossOrigin</span> : boolean|string</p>
+<p>跨域设置，此值经常被设置为'anonymous'</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-74">line 74</a><p>
+
+<p><span class="keywords">enableParallel</span> : integer</p>
+<p>并行加载，默认开启。如果要关闭，要在加载资源前，设置此值为false。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-188">line 188</a><p>
+
+<p><span class="keywords">hasLoaded</span> : boolean</p>
+<p>如果所有资源都加载完成，此值为true。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-57">line 57</a><p>
+
+<p><span class="keywords">isLoading</span> : boolean</p>
+<p>如果正在加载资源，此值为true。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-50">line 50</a><p>
+
+<p><span class="keywords">onFileComplete</span> : <a>Phaser.Signal</a></p>
+<p>当一个资源文件(file)加载成功或者加载失败时，触发此信号。它的回调函数将接受以下参数：进度、文件的key、是否成功、总共加载了多少文件、总共要加载多少文件。其中进度是一个1到100的数，表示百分比。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-152">line 152</a><p>
+
+<p><span class="keywords">onFileError</span> : <a>Phaser.Signal</a></p>
+<p>当一个资源文件(file)或者资源包(pack)加载失败时，触发此信号。如果是资源文件(file)，它会在onFileComplete之前被触发。如果是资源包(pack)，它会在onPackComplete之前被触发。</p>
+<p>参数：(file key, file)</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-163">line 163</a><p>
+
+<p><span class="keywords">onFileStart</span> : <a>Phaser.Signal</a></p>
+<p>当一个资源文件开始加载的时候，触发此信号。这个信号触发后，资源文件可能加载成功，也可能加载失败。</p>
+<p>参数：(progress, file key, file url)</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-139">line 139</a><p>
+
+<p><span class="keywords">onLoadComplete</span> : <a>Phaser.Signal</a></p>
+<p>最后一个资源加载完成或加载失败时，触发此信号。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-118">line 118</a><p>
+
+<p><span class="keywords">onLoadStart</span> : <a>Phaser.Signal</a></p>
+<p>加载进度开始计算时，触发此信号。发生于第一个资源文件被请求前，但是在包(pack)描述文件被加载后</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-111">line 111</a><p>
+
+<p><span class="keywords">onPackComplete</span> : <a>Phaser.Signal</a></p>
+<p>当一个资源包加载完成或者加载失败时，触发此信号。当一个资源包的清单(manifest)文件加载成功，并且把内容加入加载队列(loader queue)的时候触发此信号。</p>
+<p>参数：(pack key, success?, total packs loaded, total packs)</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-129">line 129</a><p>
+
+<p><span class="keywords">path</span> : string</p>
+<p>如果设置此值，将在所有相对路径前加上它。比如：</p>
+<p>load.path = "images/sprites/";
+<p>load.image("ball", "ball.png");</p>
+<p>load.image("tree", "level1/oaktree.png");</p>
+<p>load.image("boom", "http://server.com/explode.png");</p>
+<p>会从images/sprites/ball.png去加载ball，从images/sprites/level1/oaktree.png去加载tree，但是从http://server.com/explode.png去加载boom</p>
+<p>path会被加在文件名前，但是在baseURL之后，要以'/'结尾</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-103">line 103</a><p>
+
+<p><span class="keywords">progress</span> : integer</p>
+<p>取整后的进度(0-100)</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-2987">line 2987</a><p>
+
+<p><span class="keywords">progressFloat</span> : number</p>
+<p>进度(0.0-100.0)。在很多文件动态加载的情况下，进度可能会反而变小。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-2969">line 2969</a><p>
+
+<p><span class="keywords">resetLocked</span> : boolean</p>
+<p>如果此值为true，则Loader.reset不起作用。如果你要在进入加载场景前创建一个加载队列，可以用这个。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-43">line 43</a><p>
+
+<p class="title">方法</p>
+
+<p><span class="keywords">addSyncPoint(type, key)</span> : → {<a>Phaser.Loader</a>}</p>
+<p>在加载队列中给指定文件添加一个同步点。对于已经加载过的文件无效。</p>
+<p class="subTitle">参数：</p>
+<table cellspacing="0">
+	<tr align="left"><th width="20%" class="indentS">参数名</th><th width="20%">参数类型</th><th width="60%">描述</th></tr>
+	<tr class="td"><td class="params indentS">type</td><td>string</td><td>资源类型(image, audio, xml 等)。</td></tr>
+	<tr class="td"><td class="params indentS">key</td><td>string</td><td>资源索引(key)。</td></tr>
+</table>
+<p>返回：Phaser.Loader，加载器引用。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-1631">line 1631</a><p>
+
+<p><span class="keywords">atlas(key, textureURL, atlasURL, atlasData, format)</span> : → {<a>Phaser.Loader</a>}</p>
+<p>在加载队列中添加一个atlas资源文件。Atlas的素材集可以通过<a>Texture Packer</a>或者<a>Shoebox</a>来制作。如果用Texture Packer，建议勾选Trim sprite names。生成资源的时候不要选择“旋转”。</p>
+<p>你可以通过URL来提供一个json文件，也可以通过参数atlasData，传递一个json对象或者一个string。如果你传入string，会被直接JSON.parse，然后被加入到Phaser.Cache。</p>
+<p>如果提供的是URL，文件不会马上被加载，而是被放入加载队列。参数key要是唯一的，它会是Phaser.Cache中的键。</p>
+<p>可以通过<span class="params">Cache.getImage(key)</span>来获取文件。JSON文件会被自动加载解析。如果你希望控制JSON文件的解析，那就用<span class="params">Loader.text</span>。</p>
+<p>URL可以是相对路径，也可以是绝对路径。如果是相对路径，<span class="params">Loader.baseURL</span>和<span class="params">Loader.path</span>会加在它前面。</p>
+<p>如果没有传textureURL参数，Loader会自动创建一个文件名。比如key是player，textureURL没传，Loader会将URL设置为player.png。对于atlasURL来说是一样的，比如key是player，atlasURL会变成player.json。如果你不希望这样，就传参数或者传json对象。</p>
+<p class="subTitle">参数：</p>
+<table cellspacing="0">
+	<tr align="left"><th width="15%" class="indentS">参数名</th><th width="15%">参数类型</th><th width="15%">是否可选</th><th width="55%">描述</th></tr>
+	<tr class="td"><td class="params indentS">key</td><td>string</td><td></td><td>资源索引(key)。</td></tr>
+	<tr class="td"><td class="params indentS">textureURL</td><td>string</td><td>可选</td><td>texture图像URL。如果是null，会被设置为&lt;key&gt;.png，例如key是alien，URL会被设置为alien.png</td></tr>
+	<tr class="td"><td class="params indentS">atlasURL</td><td>string</td><td>可选</td><td>atlas数据文件URL。如果是null，会被设置为&lt;key&gt;.json，例如key是alien，URL会被设置为alien.json</td></tr>
+	<tr class="td"><td class="params indentS">atlasData</td><td>object</td><td>可选</td><td>JSON或者XML对象，如果指定了URL，就不需要此参数。</td></tr>
+	<tr class="td"><td class="params indentS">format</td><td>number</td><td>可选</td><td>数据格式。可以是 Phaser.Loader.TEXTURE_ATLAS_JSON_ARRAY(默认), Phaser.Loader.TEXTURE_ATLAS_JSON_HASH 或者 Phaser.Loader.TEXTURE_ATLAS_XML_STARLING</td></tr>
+</table>
+<p>返回：Phaser.Loader，加载器引用。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-1500">line 1500</a><p>
+
+<p><span class="keywords">atlasJSONArray(key, textureURL, atlasURL, atlasData)</span> : → {<a>Phaser.Loader</a>}</p>
+<p>在加载队列中添加一个atlas资源文件。希望数据是JSON数组。Atlas的素材集可以通过<a>Texture Packer</a>或者<a>Shoebox</a>来制作。如果用Texture Packer，建议勾选Trim sprite names。生成资源的时候不要选择“旋转”。</p>
+<p>你可以通过URL来提供一个json文件，也可以通过参数atlasData，传递一个json对象或者一个string。如果你传入string，会被直接JSON.parse，然后被加入到Phaser.Cache。</p>
+<p>如果提供的是URL，文件不会马上被加载，而是被放入加载队列。参数key要是唯一的，它会是Phaser.Cache中的键。</p>
+<p>可以通过<span class="params">Cache.getImage(key)</span>来获取文件。JSON文件会被自动加载解析。如果你希望控制JSON文件的解析，那就用<span class="params">Loader.text</span>。</p>
+<p>URL可以是相对路径，也可以是绝对路径。如果是相对路径，<span class="params">Loader.baseURL</span>和<span class="params">Loader.path</span>会加在它前面。</p>
+<p>如果没有传textureURL参数，Loader会自动创建一个文件名。比如key是player，textureURL没传，Loader会将URL设置为player.png。对于atlasURL来说是一样的，比如key是player，atlasURL会变成player.json。如果你不希望这样，就传参数或者传json对象。</p>
+<p class="subTitle">参数：</p>
+<table cellspacing="0">
+	<tr align="left"><th width="15%" class="indentS">参数名</th><th width="15%">参数类型</th><th width="15%">是否可选</th><th width="55%">描述</th></tr>
+	<tr class="td"><td class="params indentS">key</td><td>string</td><td></td><td>资源索引(key)。</td></tr>
+	<tr class="td"><td class="params indentS">textureURL</td><td>string</td><td>可选</td><td>texture图像URL。如果是null，会被设置为&lt;key&gt;.png，例如key是alien，URL会被设置为alien.png</td></tr>
+	<tr class="td"><td class="params indentS">atlasURL</td><td>string</td><td>可选</td><td>atlas数据文件URL。如果是null，会被设置为&lt;key&gt;.json，例如key是alien，URL会被设置为alien.json</td></tr>
+	<tr class="td"><td class="params indentS">atlasData</td><td>object</td><td>可选</td><td>JSON或者XML对象，如果指定了URL，就不需要此参数。</td></tr>
+</table>
+<p>返回：Phaser.Loader，加载器引用。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-1354">line 1354</a><p>
+
+<p><span class="keywords">atlasJSONHash(key, textureURL, atlasURL, atlasData)</span> : → {<a>Phaser.Loader</a>}</p>
+<p>在加载队列中添加一个atlas资源文件。希望数据是JSON对象。Atlas的素材集可以通过<a>Texture Packer</a>或者<a>Shoebox</a>来制作。如果用Texture Packer，建议勾选Trim sprite names。生成资源的时候不要选择“旋转”。</p>
+<p>你可以通过URL来提供一个json文件，也可以通过参数atlasData，传递一个json对象或者一个string。如果你传入string，会被直接JSON.parse，然后被加入到Phaser.Cache。</p>
+<p>如果提供的是URL，文件不会马上被加载，而是被放入加载队列。参数key要是唯一的，它会是Phaser.Cache中的键。</p>
+<p>可以通过<span class="params">Cache.getImage(key)</span>来获取文件。JSON文件会被自动加载解析。如果你希望控制JSON文件的解析，那就用<span class="params">Loader.text</span>。</p>
+<p>URL可以是相对路径，也可以是绝对路径。如果是相对路径，<span class="params">Loader.baseURL</span>和<span class="params">Loader.path</span>会加在它前面。</p>
+<p>如果没有传textureURL参数，Loader会自动创建一个文件名。比如key是player，textureURL没传，Loader会将URL设置为player.png。对于atlasURL来说是一样的，比如key是player，atlasURL会变成player.json。如果你不希望这样，就传参数或者传json对象。</p>
+<p class="subTitle">参数：</p>
+<table cellspacing="0">
+	<tr align="left"><th width="15%" class="indentS">参数名</th><th width="15%">参数类型</th><th width="15%">是否可选</th><th width="55%">描述</th></tr>
+	<tr class="td"><td class="params indentS">key</td><td>string</td><td></td><td>资源索引(key)。</td></tr>
+	<tr class="td"><td class="params indentS">textureURL</td><td>string</td><td>可选</td><td>texture图像URL。如果是null，会被设置为&lt;key&gt;.png，例如key是alien，URL会被设置为alien.png</td></tr>
+	<tr class="td"><td class="params indentS">atlasURL</td><td>string</td><td>可选</td><td>atlas数据文件URL。如果是null，会被设置为&lt;key&gt;.json，例如key是alien，URL会被设置为alien.json</td></tr>
+	<tr class="td"><td class="params indentS">atlasData</td><td>object</td><td>可选</td><td>JSON或者XML对象，如果指定了URL，就不需要此参数。</td></tr>
+</table>
+<p>返回：Phaser.Loader，加载器引用。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-1400">line 1400</a><p>
+
+<p><span class="keywords">atlasXML(key, textureURL, atlasURL, atlasData)</span> : → {<a>Phaser.Loader</a>}</p>
+<p>在加载队列中添加一个atlas资源文件。希望数据是XML格式。Atlas的素材集可以通过<a>Texture Packer</a>或者<a>Shoebox</a>来制作。如果用Texture Packer，建议勾选Trim sprite names。生成资源的时候不要选择“旋转”。</p>
+<p>你可以通过URL来提供一个json文件，也可以通过参数atlasData，传递一个json对象或者一个string。如果你传入string，会被直接JSON.parse，然后被加入到Phaser.Cache。</p>
+<p>如果提供的是URL，文件不会马上被加载，而是被放入加载队列。参数key要是唯一的，它会是Phaser.Cache中的键。</p>
+<p>可以通过<span class="params">Cache.getImage(key)</span>来获取文件。JSON文件会被自动加载解析。如果你希望控制JSON文件的解析，那就用<span class="params">Loader.text</span>。</p>
+<p>URL可以是相对路径，也可以是绝对路径。如果是相对路径，<span class="params">Loader.baseURL</span>和<span class="params">Loader.path</span>会加在它前面。</p>
+<p>如果没有传textureURL参数，Loader会自动创建一个文件名。比如key是player，textureURL没传，Loader会将URL设置为player.png。对于atlasURL来说是一样的，比如key是player，atlasURL会变成player.json。如果你不希望这样，就传参数或者传json对象。</p>
+<p class="subTitle">参数：</p>
+<table cellspacing="0">
+	<tr align="left"><th width="15%" class="indentS">参数名</th><th width="15%">参数类型</th><th width="15%">是否可选</th><th width="55%">描述</th></tr>
+	<tr class="td"><td class="params indentS">key</td><td>string</td><td></td><td>资源索引(key)。</td></tr>
+	<tr class="td"><td class="params indentS">textureURL</td><td>string</td><td>可选</td><td>texture图像URL。如果是null，会被设置为&lt;key&gt;.png，例如key是alien，URL会被设置为alien.png</td></tr>
+	<tr class="td"><td class="params indentS">atlasURL</td><td>string</td><td>可选</td><td>atlas数据文件URL。如果是null，会被设置为&lt;key&gt;.json，例如key是alien，URL会被设置为alien.json</td></tr>
+	<tr class="td"><td class="params indentS">atlasData</td><td>object</td><td>可选</td><td>JSON或者XML对象，如果指定了URL，就不需要此参数。</td></tr>
+</table>
+<p>返回：Phaser.Loader，加载器引用。</p>
+<p class="source indentS">源码 - <a href="../docs/src_loader_Loader.js.html">loader/Loader.js</a>, <a href="../docs/src_loader_Loader.js.html#sunlight-1-line-1446">line 1446</a><p>
+
 
 
 
