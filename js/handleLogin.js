2ceffce4a5052a128648c54ced86375ba1591c1a
竹逊 <zhuxun.lx@alibaba-inc.com>
2016-12-24 22:51:31 +0800

    qq login

--- js/handleLogin.js
+++ js/handleLogin.js
@@ -1,24 +1,24 @@
 $(function() {
 	
 	$("#webLoginBtn").on("click", function() {
-		if(window.location.href.indexOf("login.php") == -1) {
+		if(window.location.href.indexOf("login") == -1) {
 			document.cookie = "loginBackUrl=" + window.location.href;
 		}
 		window.location.href = "login.html";
 	});
 	
 	$("#weiboLoginBtn").on("click", function() {
-		if(window.location.href.indexOf("login.php") == -1) {
+		if(window.location.href.indexOf("login") == -1) {
 			document.cookie = "loginBackUrl=" + window.location.href;
 		}
 		window.location.href = "loginsdk/weibo/login.html";
 	})
 	
 	$("#qqLoginBtn").on("click", function() {
-		if(window.location.href.indexOf("login.php") == -1) {
+		if(window.location.href.indexOf("login") == -1) {
 			document.cookie = "loginBackUrl=" + window.location.href;
 		}
-		window.location.href = "http://www.phaser-china.com/loginsdk/qq/login.php";
+		window.location.href = "loginsdk/qq/login.html";
 	})
 	
 	$("#logoutBtn").on("click", function() {
